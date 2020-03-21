992fe60f5498102c15c6bfe6721cb05992b41c9e
Michael Kennedy <michaelkennedyns5@gmail.com>
2020-03-20 21:26:14 -0700

    Forgot disk info, waiting until Monday to clarify

--- Bash/mac-query/.helpers/get-info.sh
+++ Bash/mac-query/.helpers/get-info.sh
@@ -9,8 +9,13 @@ scutil --get ComputerName
 # OS Version
 sw_vers -productVersion
 
+# Disk Space Information
+# Main disk on iMac Machines is /dev/disk2
+# Ask Jeremy on Monday about other machines on VU network
+
 # Computer IP address
 ifconfig en0 | grep "inet " | awk '{print 2}'
+
 
 # Last user signed in
 last -1 | awk '{print $1}'
