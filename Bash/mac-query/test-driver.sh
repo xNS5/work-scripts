7426c6fe3f2f50a08d53cfe590f97d73d05ca745
Michael Kennedy <michaelkennedyns5@gmail.com>
2020-03-31 15:47:33 -0700

    Removing test files, adding app-info.sh

--- Bash/mac-query/test-driver.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-printf "Bonjour Name,Computer Name,OS Version,Available Disk Space,Total Disk Space,Percent Used,Percent Available,IP Address,Last User\n" >> output.csv
-
-var="$( bash test.sh  | tr '\n' ',')"
-echo ${var:: -1} >> output.csv
-
