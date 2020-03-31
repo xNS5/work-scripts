da33b7e331132953eac1efbeab70cbe31175a61a
Michael Kennedy <michaelkennedyns5@gmail.com>
2020-03-31 12:24:39 -0700

    Testing batch execution

--- Bash/mac-query/get-info.sh
+++ Bash/mac-query/get-info.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/bash
 
 # Bonjour Name
-echo -n "Bonjour Name: "
+#echo -n "Bonjour Name: "
 scutil --get LocalHostName
-
 # Computer Name
-echo -n "Computer Name: "
+#echo -n "Computer Name: "
 scutil --get ComputerName
-
 # OS Version
-echo -n "OS Version: "
+#echo -n "OS Version: "
 sw_vers -productVersion
-
-
-# Disk Space Information
-
-echo "DISK INFO:"
-echo 
+# Disk Space Information 
 
 AVAILABLE="$(system_profiler SPStorageDataType | grep -m1 "Available" | xargs)"
 CAPACITY="$(system_profiler SPStorageDataType | grep -m1 "Capacity" | xargs)"
@@ -24,36 +17,38 @@ availableLen="${#AVAILABLE}"
 
 if [[ $availableLen -ne 0 ]];
 then
-   echo -n "Space "
-   echo $AVAILABLE
-   echo -n "Space "
-   echo $CAPACITY
+   #Total Available
+   echo $AVAILABLE | awk '{print $2 " " $3}'
+   #Total Capacity 
+   echo $CAPACITY | awk '{print $2 " " $3}'
    BITSFREE="$(echo $AVAILABLE | awk '{print $4}' | sed  's/[^0-9]*//g')"
    BITSTOTAL="$(echo $CAPACITY | awk '{print $4}' | sed 's/[^0-9]*//g')"
-   printf "Percent Used: %.2f%s\n" "$(bc <<< "scale=4; (1-($BITSFREE/$BITSTOTAL))*100")"
-   printf "Percent Free: %.2f%s\n" "$(bc <<< "scale=4; ($BITSFREE/$BITSTOTAL)*100")"
-
+   # Percent Used
+   printf "%.2f%s\n" "$(bc <<< "scale=4; (1-($BITSFREE/$BITSTOTAL))*100")"
+   # Percent Free
+   printf "%.2f%s\n" "$(bc <<< "scale=4; ($BITSFREE/$BITSTOTAL)*100")"
+   
 else
    FREE="$(system_profiler SPStorageDataType | grep -m1 "Free" | xargs)"
-   echo -n "Space "
-   echo $FREE
-   echo -n "Total "
-   echo $CAPACITY
+   #Space Free
+   echo $FREE | awk '{print $2 " " $3}'
+   #Total Capacity
+   echo $CAPACITY | awk '{print $2 " " $3}'
+
    BITSFREE="$(echo $FREE | awk '{print $4}' | sed  's/[^0-9]*//g')"
    BITSTOTAL="$(echo $CAPACITY | awk '{print $4}' | sed 's/[^0-9]*//g')"
-   printf "Percent Used: %.2f%s\n" "$(bc <<< "scale=4; (1-($BITSFREE/$BITSTOTAL))*100")"
-   printf "Percent Free: %.2f%s\n" "$(bc <<< "scale=4; ($BITSFREE/$BITSTOTAL)*100")"
-   
+   # Percent Used
+   printf "%.2f%s\n" "$(bc <<< "scale=4; (1-($BITSFREE/$BITSTOTAL))*100")"
+   #Percent Free
+   printf "%.2f%s\n" "$(bc <<< "scale=4; ($BITSFREE/$BITSTOTAL)*100")"
 fi
-echo
 
 # Computer IP address
-echo -n "IP Address: "
+# echo -n "IP Address: "
 ifconfig en0 | grep "inet " | awk '{print $2}'
 
 
 # Last user signed in
-echo -n "Last user: "
-last -1 | awk '{print $1}'
-
+#echo -n "Last user: "
+last -2 | awk '{print $1}' | sed -n '2 p'
 
--- Bash/mac-query/mac-query.sh
+++ Bash/mac-query/mac-query.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 
-tempIP="140.160.164.232"
+#tempIP="140.160.164.232"
 
-# echo -n "Please drag and drop .txt file here: "
-# read path
-echo
-ssh vumaint@$tempIP 'bash -s' < get-info.sh
+echo -n "Please drag and drop .txt file here: "
+read path
+printf "Bonjour Name,Computer Name,OS Version,Available Disk Space,Total Disk Space,Percent Used,Percent Available,IP Address,Last User\n" >> output.csv 
+for addr in $path;
+do
+   output="$(ssh vumaint@$addr 'bash -s' < get-info.sh | tr '\n' ',')"
+   echo ${output::-1} >> output.csv
+done
