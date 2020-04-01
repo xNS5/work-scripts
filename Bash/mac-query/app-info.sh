bc51498032f4bc32ee33d6d25da2dc0e666c5808
Michael Kennedy <michaelkennedyns5@gmail.com>
2020-04-01 16:06:39 -0700

    Found efficient ways to get file information

--- Bash/mac-query/app-info.sh
+++ Bash/mac-query/app-info.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/bash
 
+#profiler="$(system_profiler -detailLevel mini SPApplicationsDataType | grep -v -e "Get Info String" -e "Signed by" -e "Last Modified" -e "Obtained From" -e "Copyright")"
+
+appNames="$(ls -1 /Applications/)"
+
+#echo $(defaults read /Aplications/Runescape.app/Contents/Info.plist CFBundleShortVersionString)
+
+for app in $appNames
+do
+   echo $app
+done
+
 
