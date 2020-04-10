3ffe952dee6db3278f9b7bd8769ba6cbab1d61f9
Michael Kennedy <michaelkennedyns5@gmail.com>
2020-04-09 22:23:09 -0700

    Made tweaks so it checks sub-directories

--- Bash/mac-query/app-info.sh
+++ Bash/mac-query/app-info.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/bash
 
 appName=""
-appVersion=""
+appVersion=0
 
 while read -r line
 do
+#  set -x
    appName=$line
-   appVersion="$(defaults read /Applications/"$line"/Contents/Info.plist "CFBundleShortVersionString" 2>/dev/null)"
+   appVersion="$(defaults read /Applications/"$appName"/Contents/Info.plist "CFBundleShortVersionString" 2>/dev/null)"
    if [[ $? -ne 0 ]]; then
       while read -r subLine
       do
-         subVersion="$(defaults read "$subline"/Contents/Info.plist "CFBundleShortVersionString")"
-         echo "$subLine"
+         subVersion="$(defaults read "$subLine"/Contents/Info.plist "CFBundleShortVersionString" 2>/dev/null)"
          if [[ $? -eq 0 ]]; then
            appVersion="$subVersion"
            appName="$subLine"
         fi
-      done< <(find $line/* -maxdepth 0 2>/dev/null | grep -e ".app")
+      done< <(find /Applications/"$line"/* -maxdepth 0 2>/dev/null | grep ".app")
    fi
 
    if [[ ${#appVersion} -ne 0 ]]; then
       printf "%s Version: %s\n" "$appName" "$appVersion"
+   else
+      printf "%s Version: N/A\n" "$appName"
    fi
-done< <( find /Applications/* -maxdepth 0 2>/dev/null | sed -e 's/\/Applications\///g' | grep -v -e "Python" | grep -v -e "Utilities")
+
+#   set +x
+done< <( find /Applications/* -maxdepth 0 2>/dev/null | sed -e 's/\/Applications\///g')
 
 
