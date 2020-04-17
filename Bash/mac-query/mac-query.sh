55d4c4b6e902f72bcb8f44f38fb4f0fb56bf920d
Michael Kennedy <michaelkennedyns5@gmail.com>
2020-04-16 22:32:28 -0700

    Outputting information to correct csv files

--- Bash/mac-query/mac-query.sh
+++ Bash/mac-query/mac-query.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/bash
 
-addr="140.160.164.232"
+#addr="140.160.164.232"
+if [[ ! $1 ]]; then
+  echo -n "Please drag and drop text file: "
+  read -r path
+else
+  path="$1"
+fi
 
-#printf "Bonjour Name,Computer Name,OS Version,Available Disk Space,Total Disk Space,Percent Used,Percent Available,IP Address,Last User\n" >> computer_info.csv
-#if [[ ! $1 ]]; then
-#  echo "Please drag and drop text file: "
-#  read -r path
-#else
-#  path="$1"
-#fi
-#while read -r addr
-#do
+printf "Bonjour Name,Computer Name,OS Version,RAM,Available Disk Space,Total Disk Space,Percent Used,Percent Available,IP Address,Last User\n" >> computer_info.csv
+printf "Computer Name,Application Name, Application Version\n" >> application_info.csv
+
+while read -r addr
+do
+   #set -x
    output="$(ssh vumaint@"$addr" 'bash -s' < computer_info.sh)"
-   deviceName="$(echo "$output" | sed -n '2 p')"
-   deviceInfo="$("")"
-
-#done < "$path"
+   echo "$output" | sed -n '1 p' >> computer_info.csv
+   echo "$output" | sed -n '2,$p' >> application_info.csv
+   #set -x
+done < "$path"
