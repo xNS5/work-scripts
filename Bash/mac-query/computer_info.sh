0e10f6e1b2bb98b3f53df2c86bf4bbfbff34afe1
Michael Kennedy <michaelkennedyns5@gmail.com>
2020-04-16 22:31:53 -0700

    Formatted output for CSV

--- Bash/mac-query/computer_info.sh
+++ Bash/mac-query/computer_info.sh
@@ -1,57 +1,59 @@
 #!/usr/bin/bash
 
 # Bonjour Name
-scutil --get LocalHostName
+printf "%s," "$(scutil --get LocalHostName)"
 
 # Computer Name
-scutil --get ComputerName
+printf "%s," "$(scutil --get ComputerName)"
 
 # OS Version
-sw_vers -productVersion
+printf "%s," "$(sw_vers -productVersion)"
 
+# RAM
+printf "%s," "$(system_profiler SPHardwareDataType | grep "Memory:" | awk '{print $2 $3}')"
 #=======================================================================================================================
 # Disk Space Information 
 #=======================================================================================================================
-AVAILABLE="$(system_profiler SPStorageDataType | grep -m1 "Available" | xargs)"
-CAPACITY="$(system_profiler SPStorageDataType | grep -m1 "Capacity" | xargs)"
-availableLen="${#AVAILABLE}"
+available="$(system_profiler SPStorageDataType | grep -m1 "Available" | xargs)"
+capacity="$(system_profiler SPStorageDataType | grep -m1 "Capacity" | xargs)"
+availableLen="${#available}"
 
 # MacOS Catalina differs from Mojave in that it uses "free" instead of "available" when doing a system_profiler query
 if [[ $availableLen -ne 0 ]];
 then
    #Total available space
-   echo $AVAILABLE | awk '{print $2 " " $3}'
+   printf "%s," "$( echo "$available" | awk '{print $2 $3}' )"
    #Total space capacity
-   echo $CAPACITY | awk '{print $2 " " $3}'
-   BITSFREE="$(echo $AVAILABLE | awk '{print $4}' | sed  's/[^0-9]*//g')"
-   BITSTOTAL="$(echo $CAPACITY | awk '{print $4}' | sed 's/[^0-9]*//g')"
+   printf "%s," "$(echo "$capacity" | awk '{print $2 $3}')"
+   BITSFREE="$(echo "$available" | awk '{print $4}' | sed  's/[^0-9]*//g')"
+   BITSTOTAL="$(echo "$capacity" | awk '{print $4}' | sed 's/[^0-9]*//g')"
    # Percent Used
-   printf "%.2f%s\n" "$(bc <<< "scale=4; (1-($BITSFREE/$BITSTOTAL))*100")"
+   printf "%.2f," "$(bc <<< "scale=4; (1-($BITSFREE/$BITSTOTAL))*100")"
    # Percent Free
-   printf "%.2f%s\n" "$(bc <<< "scale=4; ($BITSFREE/$BITSTOTAL)*100")"
+   printf "%.2f," "$(bc <<< "scale=4; ($BITSFREE/$BITSTOTAL)*100")"
    
 else
-   FREE="$(system_profiler SPStorageDataType | grep -m1 "Free" | xargs)"
+   free="$(system_profiler SPStorageDataType | grep -m1 "Free" | xargs)"
    #Space Free
-   echo $FREE | awk '{print $2 " " $3}'
+   printf "%s," "$(echo "$free" | awk '{print $2 $3}')"
    #Total Capacity
-   echo $CAPACITY | awk '{print $2 " " $3}'
+   echo $capacity| awk '{print $2 $3}'
 
-   BITSFREE="$(echo $FREE | awk '{print $4}' | sed  's/[^0-9]*//g')"
-   BITSTOTAL="$(echo $CAPACITY | awk '{print $4}' | sed 's/[^0-9]*//g')"
+   BITSFREE="$(echo "$free" | awk '{print $4","}' | sed  's/[^0-9]*//g')"
+   BITSTOTAL="$(echo "$capacity" | awk '{print $4","}' | sed 's/[^0-9]*//g')"
    # Percent Used
-   printf "%.2f%s\n" "$(bc <<< "scale=4; (1-($BITSFREE/$BITSTOTAL))*100")"
+   printf "%.2f," "$(bc <<< "scale=4; (1-($BITSFREE/$BITSTOTAL))*100")"
    #Percent Free
-   printf "%.2f%s\n" "$(bc <<< "scale=4; ($BITSFREE/$BITSTOTAL)*100")"
+   printf "%.2f," "$(bc <<< "scale=4; ($BITSFREE/$BITSTOTAL)*100")"
 fi
 
 #=======================================================================================================================
 
 # Computer IP address
-ifconfig en0 | grep "inet " | awk '{print $2}'
+printf "%s," "$(ifconfig en0 | grep "inet " | awk '{print $2}')"
 
 # Last user signed in
-last -2 | grep -v -e 'reboot' -e 'shutdown' | awk '{print $1}' | sed -n '2 p'
+printf "%s\n" "$(last -2 | grep -v -e 'reboot' -e 'shutdown' | awk '{print $1}' | sed -n '2 p')"
 
 #=======================================================================================================================
 # Application Information: Name and Version
@@ -60,6 +62,8 @@ last -2 | grep -v -e 'reboot' -e 'shutdown' | awk '{print $1}' | sed -n '2 p'
 appName=""
 appVersion=0
 
+# Printing out the computer name to associate apps with a computer
+printf "%s" "$(scutil --get ComputerName)"
 # Line is the name of an application located in a computer's /Applications folder
 while read -r line
 do
@@ -69,15 +73,15 @@ do
 
    # appVersion is the captured output from defaults read... and checks for number associated with "CFBundleShortVersionString".
    # 2>/dev/null suppresses errors in the event that a CFBundleShortVersionString isn't found
-   appVersion="$(defaults read /Applications/"$line"/Contents/Info.plist "CFBundleShortVersionString" 2>/dev/null)"
 
    # $? checks the exit status of the previously run command and moves 1 level deeper into .app file.
    # This is required for Adobe and some other applications.
-   if [[ $? -ne 0 ]]; then
+   appVersion="$(defaults read /Applications/"$line"/Contents/Info.plist "CFBundleShortVersionString" 2>/dev/null)"
+   if [[ ! $appVersion ]]; then
       while read -r subLine
       do
          subVersion="$(defaults read "$subLine"/Contents/Info.plist "CFBundleShortVersionString" 2>/dev/null)"
-         if [[ $? -eq 0 ]]; then
+         if [[ $subVersion ]]; then
            appVersion="$subVersion"
            # Cleaning up the application's name
            appName="$(echo "$subLine" | sed -E 's/(\/Applications\/)(.*\/)//g')"
@@ -87,13 +91,15 @@ do
 
   # Removing .app extension
   # Note: Some computers have an extension called ".appdownload".
-   appName="${appName//.app/}"
+  if [[ $appName != *".appdownload" ]]; then
+    appName="${appName//.app/}"
+   fi
    # Checks if both appVersion and appName are valid
    if [[ $appVersion && $appName ]]; then
-      printf "%s %s\n" "$appName" "$appVersion"
+      printf ",%s,%s\n" "$appName" "$appVersion"
    else
-      printf "%s N/A\n" "$appName"
+      printf ",%s,N/A\n" "$appName"
    fi
 #   set +x
-done< <( find /Applications/* -maxdepth 0 2>/dev/null | sed -e 's/\/Applications\///g' )
+done< <( find /Applications/* -maxdepth 0 2>/dev/null | sed -e 's/\/Applications\///g')
 
