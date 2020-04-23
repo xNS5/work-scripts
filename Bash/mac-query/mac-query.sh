092f606d97015fc8dc40898ac1cba6820f605674
Michael Kennedy <michaelkennedyns5@gmail.com>
2020-04-22 19:59:24 -0700

    Fixed issue with newlines being parsed as input

--- Bash/mac-query/mac-query.sh
+++ Bash/mac-query/mac-query.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/bash
 
 #set -x
-#if [[ $1 ]]; then
-#    file=$1
-#else
-#  echo -n "Please drag and drop text file: "
-#  read -r file
-#fi
-Sanitizing Input
+if [[ $1 ]]; then
+    file=$1
+else
+  echo -n "Please drag and drop text file: "
+  read -r file
+fi
 
+printf "Bonjour Name,Computer Name,OS Version,RAM,Available Disk Space,Total Disk Space,Percent Used,Percent Available,IP Address,Last User\n" >> computer_info.csv
+printf "Computer Name,Application Name, Application Version\n" >> application_info.csv
 
-#printf "Bonjour Name,Computer Name,OS Version,RAM,Available Disk Space,Total Disk Space,Percent Used,Percent Available,IP Address,Last User\n" >> computer_info.csv
-#printf "Computer Name,Application Name, Application Version\n" >> application_info.csv
-
-while read -r addr
+overallStart="$(date +%s)"
+while IFS= read -r addr || [ -n "$addr" ];
 do
-      if [[ ${#addr} -ne 0 ]]; then
         printf "Beginning Query On: %s\n" "$addr"
         start="$(date +%s)"
         output="$(ssh vumaint@"$addr" 'bash -s' < computer_info.sh)"
         echo "$output" | sed -n '1 p' >> computer_info.csv
         echo "$output" | sed -n '2,$p' >> application_info.csv
         end="$(date +%s)"
-        printf "Completed: $addr\n Time: %s seconds\n" "$((end-start))"
-    fi
+        printf "Completed: $addr\nTime: %s seconds\n" "$((end-start))"
 done < "$file"
+overallEnd="$(date +%s)"
 #set +x
+
+printf "Finished in: %s seconds" "$((overallEnd-overallStart))"
